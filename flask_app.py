926b31b966073a9b5a2824d9826a3e46be92274a
kuche <guschingb@yandexlyceum.ru>
2021-04-07 22:35:40 +0300

    start to develop bulls and cows

--- flask_app.py
+++ flask_app.py
@@ -6,7 +6,7 @@ import os
 
 app = Flask(__name__)
 
-
+logging.basicConfig(filename='example.log')
 logging.basicConfig(level=logging.INFO)
 
 sessionStorage = {}
@@ -115,3 +115,4 @@ def get_suggests(user_id):
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
