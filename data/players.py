a29cfaadfefa65d8b6ee0f72f6aa23dedf50fd75
kuche <guschingb@yandexlyceum.ru>
2021-04-12 23:55:32 +0300

    added kind of logging system

--- data/players.py
+++ data/players.py
@@ -3,13 +3,13 @@ import sqlalchemy
 from .db_session import SqlAlchemyBase
 
 
-class PLayer(SqlAlchemyBase):
+class Player(SqlAlchemyBase):
     __tablename__ = 'players'
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
-    login = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    login = sqlalchemy.Column(sqlalchemy.String, nullable=False, unique=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.BIGINT, nullable=False)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                      default=datetime.datetime.now)
     score = sqlalchemy.Column(sqlalchemy.Integer, default=0)
--- for_local_debugging.py
+++ for_local_debugging.py
@@ -1,15 +1,44 @@
 from flask import Flask
 from data import db_session
+from data.players import Player
 
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
+def add_player(info):
+    player = Player()
+    player.login = info[0]
+    player.hashed_password = hash(info[1])
+    db_sess = db_session.create_session()
+    try:
+        db_sess.add(player)
+        db_sess.commit()
+    except Exception as ex:
+        print(ex)
+    else:
+        print('ok')
+
+
+def login(info):
+    db_sess = db_session.create_session()
+    user = db_sess.query(Player).filter(Player.login == info[0], Player.hashed_password == hash(info[1])).first()
+    print(user)
+
 
 def main():
     db_session.global_init("db/game.db")
-    app.run()
+    #app.run()
+    answ = input('A u new: y/n')
+    if answ == 'y':
+        log_pasw = input('Login passw').split()
+        add_player(log_pasw)
+    else:
+        log_pasw = input('Login passw').split()
+        login(log_pasw)
+
+
 
 
 if __name__ == '__main__':
